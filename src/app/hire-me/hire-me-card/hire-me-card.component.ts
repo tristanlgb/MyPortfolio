a7c61fe5135dead645d5dd1a9a8e09b6382939c7
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2024-03-18 00:37:39 -0300

    Add files via upload

---
+++ src/app/hire-me/hire-me-card/hire-me-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hire-me-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './hire-me-card.component.html',
+  styleUrl: './hire-me-card.component.css'
+})
+export class HireMeCardComponent {
+
+}
---
+++ src/app/more-about/more-about-card/more-about-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-more-about-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './more-about-card.component.html',
+  styleUrl: './more-about-card.component.css'
+})
+export class MoreAboutCardComponent {
+
+}
